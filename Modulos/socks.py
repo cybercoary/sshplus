856a7513e6baabddfba69a5f1ad9ef1b756c9e51
cybercoary <165339153+cybercoary@users.noreply.github.com>
2024-08-27 22:15:38 -0400

    Add files via upload

--- Modulos/socks.py
+++ Modulos/socks.py
@@ -1,387 +1,116 @@
-import socket
-import ssl
-import select
+import socket,re
 import threading
-import os
-import argparse
-import logging
-import resource
-
-from abc import abstractproperty, ABCMeta, abstractmethod
-from typing import List, Tuple, Union, Optional
-
-__author__ = 'Jonildo F Roque'
-__version__ = '0.0.0'
-
-resource.setrlimit(resource.RLIMIT_NOFILE, (65536, 65536))
-
-logger = logging.getLogger(__name__)
-
-WS_DEFAULT_RESPONSE = b'\r\n'.join(
-    [
-        b'HTTP/1.1 101 @cybercoari',
-        b'\r\n',
-    ]
-)
-
-HTTP_DEFAULT_RESPONSE = b'\r\n'.join(
-    [
-        b'HTTP/1.1 200 @cybercoari',
-        b'\r\n',
-    ]
-)
-
-
-class ResponseParser(metaclass=ABCMeta):
-    @abstractmethod
-    def parse(self, data: bytes) -> bytes:
-        raise NotImplementedError
-
-
-class ResponseParserAbstract(ResponseParser):
-    def __init__(self, next: Union[ResponseParser, None] = None):
-        self.next = next
-
-    def parse(self, data: bytes) -> bytes:
-        if self.next:
-            return self.next.parse(data)
-        return data
-
-
-class WebsocketParseResponse(ResponseParserAbstract):
-    __contains = (b'upgrade: websocket', b'upgrade: ws')
-
-    def parse(self, data: bytes) -> bytes:
-        for line in data.split(b'\r\n'):
-            if line.lower() in self.__contains:
-                return WS_DEFAULT_RESPONSE
-        return super().parse(data)
-
-
-class HttpParseResponse(ResponseParserAbstract):
-    def parse(self, data: bytes) -> bytes:
-        return HTTP_DEFAULT_RESPONSE
-
-
-class ConnectionTypeParser:
-    def __init__(self, type: bytes, address: Tuple[str, int]):
-        self._type = type
-        self._address = address
-
-    @property
-    def type(self) -> bytes:
-        return self._type
-
-    @property
-    def address(self) -> Tuple[str, int]:
-        return self._address
-
-    @abstractproperty
-    def name(self) -> str:
-        raise NotImplementedError
-
-    def is_valid(self, data: bytes) -> bool:
-        return data.startswith(self._type)
-
-
-class SSHConnectionType(ConnectionTypeParser):
-    def __init__(self) -> None:
-        super().__init__(b'SSH-', ('0.0.0.0', 22))
-
-    @property
-    def name(self) -> str:
-        return 'SSH'
-
-
-
-class Connection:
-    def __init__(self, conn: Union[socket.socket, ssl.SSLSocket], addr: Tuple[str, int]):
-        self.__conn = conn
-        self.__addr = addr
-        self.__buffer = b''
-        self.__closed = False
-
-    @property
-    def conn(self) -> Union[socket.socket, ssl.SSLSocket]:
-        if not isinstance(self.__conn, (socket.socket, ssl.SSLSocket)):
-            raise TypeError('Connection is not a socket')
-
-        if not self.__conn.fileno() > 0:
-            raise ConnectionError('Connection is closed')
-
-        return self.__conn
-
-    @conn.setter
-    def conn(self, conn: Union[socket.socket, ssl.SSLSocket]):
-        if not isinstance(conn, (socket.socket, ssl.SSLSocket)):
-            raise TypeError('Connection is not a socket')
-
-        if not conn.fileno() > 0:
-            raise ConnectionError('Connection is closed')
-
-        self.__conn = conn
-
-    @property
-    def addr(self) -> Tuple[str, int]:
-        return self.__addr
-
-    @addr.setter
-    def addr(self, addr: Tuple[str, int]):
-        self.__addr = addr
-
-    @property
-    def buffer(self) -> bytes:
-        return self.__buffer
-
-    @buffer.setter
-    def buffer(self, data: bytes) -> None:
-        self.__buffer = data
-
-    @property
-    def closed(self) -> bool:
-        return self.__closed
-
-    @closed.setter
-    def closed(self, value: bool) -> None:
-        self.__closed = value
-
-    def close(self):
-        self.conn.close()
-        self.closed = True
-
-    def read(self, size: int = 4096) -> Optional[bytes]:
-        data = self.conn.recv(size)
-        return data if len(data) > 0 else None
-
-    def write(self, data: Union[bytes, str]) -> int:
-        if isinstance(data, str):
-            data = data.encode()
-
-        if len(data) <= 0:
-            raise ValueError('Write data is empty')
-
-        return self.conn.send(data)
-
-    def queue(self, data: Union[bytes, str]) -> int:
-        if isinstance(data, str):
-            data = data.encode()
-
-        if len(data) <= 0:
-            raise ValueError('Queue data is empty')
-
-        self.__buffer += data
-        return len(data)
-
-    def flush(self) -> int:
-        sent = self.write(self.__buffer)
-        self.__buffer = self.__buffer[sent:]
-        return sent
-
-
-class Client(Connection):
-    def __str__(self):
-        return 'Cliente - %s:%s' % self.addr
-
-
-class Server(Connection):
-    def __str__(self):
-        return 'Servidor - %s:%s' % self.addr
-
-    @classmethod
-    def of(cls, addr: Tuple[str, int]) -> 'Server':
-        return cls(socket.socket(socket.AF_INET, socket.SOCK_STREAM), addr)
-
-    def connect(self, addr: Tuple[str, int] = None, timeout: int = 5) -> None:
-        self.addr = addr or self.addr
-        self.conn = socket.create_connection(self.addr, timeout)
-        self.conn.settimeout(None)
-
-        logger.debug('%s Conexão estabelecida' % self)
-
-
-class Proxy(threading.Thread):
-    def __init__(self, client: Client, server: Optional[Server] = None) -> None:
-        super().__init__()
-        self.client = client
-        self.server = server
-        self.__running = False
-        self.__http_response_parser = WebsocketParseResponse(HttpParseResponse())
-
-    @property
-    def running(self) -> bool:
-        if self.server and self.server.closed and self.client.closed:
-            self.__running = False
-        return self.__running
-
-    @running.setter
-    def running(self, value: bool) -> None:
-        self.__running = value
-
-    def _process_request(self, data: bytes) -> None:
-        if self.server and not self.server.closed:
-            self.server.queue(data)
-            return
-
-        connection_type = SSHConnectionType()
-
-        if connection_type.is_valid(data):
-            if connection_type:
-                logger.info(
-                    '%s -> Modo %s - %s:%s',
-                    self.client,
-                    connection_type.name,
-                    *connection_type.address,
-                )
-                self.server = Server.of(connection_type.address)
-                self.server.connect()
-                self.server.queue(data)
-                return
-
-        logger.info(
-            '%s -> Solicitação: %s',
-            self.client,
-            data,
-        )
-        self.client.queue(self.__http_response_parser.parse(data))
-
-    def _get_waitable_lists(self) -> Tuple[List[socket.socket]]:
-        r, w, e = ([self.client.conn], [], [])  # type: ignore
-
-        if self.server and not self.server.closed:
-            r.append(self.server.conn)
-
-        if self.client.buffer:
-            w.append(self.client.conn)
-
-        if self.server and not self.server.closed and self.server.buffer:
-            w.append(self.server.conn)
-
-        return r, w, e  # type: ignore
-
-    def _process_wlist(self, wlist: List[socket.socket]) -> None:
-        if self.client.conn in wlist:
-            sent = self.client.flush()
-            logger.debug('%s -> enviado %s bytes' % (self.client, sent))
-
-        if self.server and not self.server.closed and self.server.conn in wlist:
-            sent = self.server.flush()
-            logger.debug('%s -> enviado %s bytes' % (self.server, sent))
-
-    def _process_rlist(self, rlist: List[socket.socket]) -> None:
-        if self.client.conn in rlist:
-            data = self.client.read(8192)
-            self.running = data is not None
-            if data and self.running:
-                self._process_request(data)
-                logger.debug('%s -> recebido %s bytes' % (self.client, len(data)))
-
-        if self.server and not self.server.closed and self.server.conn in rlist:
-            data = self.server.read(8192)
-            self.running = data is not None
-            if data and self.running:
-                self.client.queue(data)
-                logger.debug('%s -> recebido %s bytes' % (self.server, len(data)))
-
-    def _process(self) -> None:
-        self.running = True
-
-        while self.running:
-            rlist, wlist, xlist = self._get_waitable_lists()  # type: ignore
-            r, w, _ = select.select(rlist, wlist, xlist, 1)  # type: ignore
-
-            self._process_wlist(w)
-            self._process_rlist(r)
-
-    def run(self) -> None:
-        try:
-            logger.info('%s conectado' % self.client)
-            self._process()
-        except Exception as e:
-            logger.exception('%s Erro: %s' % (self.client, e))
-        finally:
-            self.client.close()
-            if self.server and not self.server.closed:
-                self.server.close()
-
-            logger.info('%s desconectado' % self.client)
-
-
-class TCP:
-    def __init__(self, addr: Tuple[str, int], backlog: int = 5):
-        self.__addr = addr
-        self.__backlog = backlog
-
-        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    def __str__(self) -> str:
-        return '%s - %s:%d' % (self.__class__.__name__, *self.__addr)
-
-    def handle(self, conn: socket.socket, addr: Tuple[str, int]) -> None:
-        raise NotImplementedError()
-
-    def run(self) -> None:
-        self.__sock.bind(self.__addr)
-        self.__sock.listen(self.__backlog)
-
-        logger.info('Servidor %s iniciado' % self)
-
-        try:
-            while True:
-                conn, addr = self.__sock.accept()
-                self.handle(conn, addr)
-        except KeyboardInterrupt:
-            pass
-        finally:
-            logger.info('Finalizando servidor...')
-            self.__sock.close()
-
-
-class HTTP(TCP):
-    def handle(self, conn: socket.socket, addr: Tuple[str, int]) -> None:
-        client = Client(conn, addr)
-        proxy = Proxy(client)
-        proxy.daemon = True
-        proxy.start()
-
-def main():
-    parser = argparse.ArgumentParser(description='Proxy', usage='%(prog)s [options]')
-
-    parser.add_argument('--host', default='0.0.0.0', help='Host')
-    parser.add_argument('--port', type=int, default=8080, help='Port')
-    parser.add_argument('--backlog', type=int, default=5, help='Backlog')
-    parser.add_argument('--ssh-port', type=int, default=22, help='SSH Port')
-
-    parser.add_argument('--cert', default='./cert.pem', help='Certificate')
-
-    parser.add_argument('--http', action='store_true', help='HTTP')
-    parser.add_argument('--https', action='store_true', help='HTTPS')
-
-    parser.add_argument('--log', default='INFO', help='Log level')
-    parser.add_argument('--usage', action='store_true', help='Usage')
-
-    args = parser.parse_args()
-
-
-    if args.http:
-        server = HTTP((args.host, args.port), args.backlog)
-
-    elif args.https:
-        if not os.path.exists(args.cert):
-            parser.error('Certificate %s not found' % args.cert)
-
-        server = HTTPS((args.host, args.port), args.cert, args.backlog)
-    else:
-        server = HTTP((args.host, args.port), args.backlog)
-
-    logging.basicConfig(
-        level=getattr(logging, args.log.upper()),
-        format='[%(asctime)s] %(levelname)s: %(message)s',
-        datefmt='%H:%M:%S',
-    )
-
-    server.run()
-
-
-if __name__ == '__main__':
-    main()
+import sys
+import ipaddress
+from os import system as s
+bg=''
+
+G = bg+'\033[32m'
+O = bg+'\033[33m'
+GR = bg+'\033[37m'
+R = bg+'\033[31m'
+
+cg = '\033[92m'
+cp = '\033[35m'
+clb = '\033[94m'
+cb = '\033[34m'
+k = '\033[0m'
+clr = '\033[91m'
+ver = '@Ver 1.0 LTS'
+		
+def save(x):
+	with open('status101.txt','a') as fl:
+		fl.write(str(x)+'\n')
+		fl.close()
+		
+def scanner(host,domain):
+	sock=socket.socket()
+	sock.settimeout(2)
+	try:
+		sock.connect((str(host),80))
+		payload='GET / HTTP/1.1\r\nHost: {}\r\n\r\n'.format(host)
+		sock.send(payload.encode())
+		response=sock.recv(1024).decode('utf-8','ignore')
+		for data in response.split('\r\n'):
+			data=data.split(':')
+			if re.match(r'HTTP/\d(\.\d)?' ,data[0]):
+				print('response status : {}{}{}'.format(O,data[0],GR))
+			if data[0]=='Server':
+				try:
+					if data[1] ==' cloudflare':
+						print('{}server : {}\nFound working {}..'.format(G,host,GR))
+						save(f'{host} === opened')
+						payloadsnd(host,domain)
+				except Exception as e:
+					print(e)
+	except Exception as e:print(e)
+
+def auto_replace(server,ip):
+	packet = server.recv(1024).decode('utf-8','ignore')
+	status = packet.split('\n')[0]
+	if re.match(r'HTTP/\d(\.\d)? 101',status):
+		print(f'{O}[TCP] response : {G}{status}{GR}')
+		save(f'{ip} response ==== {status}')
+	else:
+		if re.match(r'HTTP/\d(\.\d)? \d\d\d ',status):
+			server.send(b'HTTP/1.1 200 Connection established\r\n\r\n')
+			print(f'{O}[TCP] response : {R}{status}{GR}')
+			return auto_replace(server,ip)
+	print('==== ==== ==== ==== ==== ==== ==== ==== ==== ====')
+
+def payloadsnd(ip,domain):
+	#domain = 'premium01.conecta4g.xyz'
+	port =80
+	sc=socket.socket()
+	sc.connect((str(ip),port))
+	payload=f'GET / HTTP/1.0[crlf]Host: {domain}[crlf][crlf]'
+	payload=payload.replace('[crlf]','\r\n')
+	sc.send(payload.encode())
+	auto_replace(sc,ip)
+
+def Main():
+	try:
+		domain = input(
+			f'{clb}[ {cp}01 {clb}]{k} Digite o Dominio \n{cg}|\n└──{k}Dominio {cg}:{k} ')
+		print()
+		range = input(
+			f'{clb}[ {cp}02 {clb}]{k} Digite Ip e intervalo no formato " 192.0.0.0/24 " \n{cg}|\n└──{k}IP {cg}:{k} ')
+		if range == '':
+			print('Noob sofre')
+			sys.exit(1)
+	except:
+		print('mando pegar o pacote você ferra tudo')
+		sys.exit(1)
+
+	try:
+		net4 = ipaddress.ip_network(range)
+	except ValueError as e:
+		print(f'\n{clr}fodeu{k}: ' + str(e))
+		print()
+		sys.exit(1)
+
+	v = 0
+	for x in net4.hosts():
+		v = v + 1
+
+	print(f'\n{clb}DIGITALIZANDO{k}: {range}\n{clb}Total de hosts no intervalo{k}: {v}\n')
+	
+	enter = input(
+			f'{clb}{cp} Enter para continuar {cg}:{k} ')
+
+	y=0
+	for host in net4.hosts():
+		try:
+			y = y + 1
+			print(' {} | {} '.format(y,v))
+			print(
+			f'{clb}{cp}IP {cg}: {host}{k} ')
+			sc=threading.Thread(scanner(host,domain))
+			sc.start()
+		except KeyboardInterrupt:
+			break
+
+	
+if __name__=="__main__":
+	s('clear')
+	Main()
